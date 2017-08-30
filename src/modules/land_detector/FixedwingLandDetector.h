50509d6ca293d5a5c45a4c6d8575436ae6e32b24
Daniel Agar <daniel@agar.ca>
2017-08-30 14:59:26 -0400

    delete control_state and cleanup vehicle_attitude

--- src/modules/land_detector/FixedwingLandDetector.h
+++ src/modules/land_detector/FixedwingLandDetector.h
@@ -42,9 +42,10 @@
 
 #pragma once
 
-#include <uORB/topics/control_state.h>
 #include <uORB/topics/actuator_armed.h>
 #include <uORB/topics/airspeed.h>
+#include <uORB/topics/sensor_corrected.h>
+#include <uORB/topics/vehicle_local_position.h>
 
 #include "LandDetector.h"
 
@@ -85,13 +86,15 @@ private:
 		float maxIntVelocity;
 	} _params{};
 
-	int _controlStateSub{-1};
 	int _armingSub{-1};
 	int _airspeedSub{-1};
+	int _sensor_corrected_sub{-1};
+	int _local_pos_sub{-1};
 
-	control_state_s _controlState{};
 	actuator_armed_s _arming{};
 	airspeed_s _airspeed{};
+	sensor_corrected_s _sensors{};
+	vehicle_local_position_s _local_pos{};
 
 	float _velocity_xy_filtered{0.0f};
 	float _velocity_z_filtered{0.0f};
